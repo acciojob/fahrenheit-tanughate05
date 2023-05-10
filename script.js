ad17c342e10f7eaa0ba0e3f20797819cc20478af
tanughate05 <93458822+tanughate05@users.noreply.github.com>
2023-05-10 11:59:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function toFahrenheit(number)
+	{
+		var ftemp= number;
+		  var fToCel = (fTemp - 32) * 5 / 9;
+  var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+    console.log(message);
+	}
